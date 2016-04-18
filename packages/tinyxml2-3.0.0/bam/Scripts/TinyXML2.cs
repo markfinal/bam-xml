c15062f10f4e683897edc2d3fb481cc9b894cacf
Mark Final <markfinal@hotmail.com>
2016-04-18 12:59:38 +0100

    [packages,tinyxml2,3.0.0] Added a dynamic library module

--- packages/tinyxml2-3.0.0/bam/Scripts/TinyXML2.cs
+++ packages/tinyxml2-3.0.0/bam/Scripts/TinyXML2.cs
@@ -27,6 +27,7 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endregion // License
+using Bam.Core;
 namespace TinyXML2
 {
     [Bam.Core.ModuleGroup("Thirdparty/TinyXML2")]
@@ -49,6 +50,61 @@ namespace TinyXML2
                         compiler.IncludePaths.AddUnique(this.CreateTokenizedString("$(packagedir)"));
                     }
                 });
+        }
+    }
+
+    [Bam.Core.ModuleGroup("Thirdparty/TinyXML2")]
+    sealed class TinyXML2Dynamic :
+        C.Cxx.DynamicLibrary
+    {
+        protected override void
+        Init(
+            Bam.Core.Module parent)
+        {
+            base.Init(parent);
+
+            this.CreateHeaderContainer("$(packagedir)/*.h");
+            var source = this.CreateCxxSourceContainer("$(packagedir)/tinyxml2.cpp");
+            source.PrivatePatch(settings =>
+                {
+                    if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.Windows))
+                    {
+                        var compiler = settings as C.ICommonCompilerSettings;
+                        compiler.PreprocessorDefines.Add("TINYXML2_EXPORT");
+                    }
+                    else
+                    {
+                        // brute force visibility
+                        var gccCompiler = settings as GccCommon.ICommonCompilerSettings;
+                        if (null != gccCompiler)
+                        {
+                            gccCompiler.Visibility = GccCommon.EVisibility.Default;
+                        }
+                        var clangCompiler = settings as ClangCommon.ICommonCompilerSettings;
+                        if (null != clangCompiler)
+                        {
+                            clangCompiler.Visibility = ClangCommon.EVisibility.Default;
+                        }
+                    }
+                });
+
+            this.PublicPatch((settings, appliedTo) =>
+                {
+                    var compiler = settings as C.ICommonCompilerSettings;
+                    if (null != compiler)
+                    {
+                        compiler.IncludePaths.AddUnique(this.CreateTokenizedString("$(packagedir)"));
+                        if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.Windows) && appliedTo != this)
+                        {
+                            compiler.PreprocessorDefines.Add("TINYXML2_IMPORT");
+                        }
+                    }
+                });
+
+            if (this.Linker is VisualCCommon.LinkerBase)
+            {
+                this.LinkAgainst<WindowsSDK.WindowsSDK>();
+            }
         }
     }
 }
