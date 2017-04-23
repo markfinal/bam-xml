3bad4279d8d5863a00876968644fbea8de34dc93
Mark Final <markfinal@hotmail.com>
2017-04-23 09:07:36 +0100

    [packages] Added tinyxml2-4.0.1, now including the test

---
+++ packages/tinyxml2-4.0.1/bam/Scripts/TinyXML2.cs
@@ -0,0 +1,256 @@
+#region License
+// Copyright (c) 2010-2017, Mark Final
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of BuildAMation nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#endregion // License
+using Bam.Core;
+namespace TinyXML2
+{
+    [Bam.Core.ModuleGroup("Thirdparty/TinyXML2")]
+    class TinyXML2Static :
+        C.StaticLibrary
+    {
+        protected override void
+        Init(
+            Bam.Core.Module parent)
+        {
+            base.Init(parent);
+
+            this.CreateHeaderContainer("$(packagedir)/*.h");
+            var source = this.CreateCxxSourceContainer("$(packagedir)/tinyxml2.cpp");
+            source.PrivatePatch(settings =>
+                {
+                    var cxxCompiler = settings as C.ICxxOnlyCompilerSettings;
+                    cxxCompiler.ExceptionHandler = C.Cxx.EExceptionHandler.Asynchronous;
+                    cxxCompiler.LanguageStandard = C.Cxx.ELanguageStandard.Cxx11;
+
+                    var visualCCompiler = settings as VisualCCommon.ICommonCompilerSettings;
+                    if (null != visualCCompiler)
+                    {
+                        visualCCompiler.WarningLevel = VisualCCommon.EWarningLevel.Level4;
+                    }
+
+                    var mingwCompiler = settings as MingwCommon.ICommonCompilerSettings;
+                    if (null != mingwCompiler)
+                    {
+                        mingwCompiler.AllWarnings = true;
+                        mingwCompiler.ExtraWarnings = true;
+                        mingwCompiler.Pedantic = true;
+                    }
+
+                    var gccCompiler = settings as GccCommon.ICommonCompilerSettings;
+                    if (null != gccCompiler)
+                    {
+                        gccCompiler.AllWarnings = true;
+                        gccCompiler.ExtraWarnings = true;
+                        gccCompiler.Pedantic = true;
+                    }
+
+                    var clangCompiler = settings as ClangCommon.ICommonCompilerSettings;
+                    if (null != clangCompiler)
+                    {
+                        clangCompiler.AllWarnings = true;
+                        clangCompiler.ExtraWarnings = true;
+                        clangCompiler.Pedantic = true;
+                    }
+                });
+
+            this.PublicPatch((settings, appliedTo) =>
+                {
+                    var compiler = settings as C.ICommonCompilerSettings;
+                    if (null != compiler)
+                    {
+                        compiler.IncludePaths.AddUnique(this.CreateTokenizedString("$(packagedir)"));
+                    }
+                });
+        }
+    }
+
+    [Bam.Core.ModuleGroup("Thirdparty/TinyXML2")]
+    class TinyXML2Dynamic :
+        C.Cxx.DynamicLibrary
+    {
+        protected override void
+        Init(
+            Bam.Core.Module parent)
+        {
+            base.Init(parent);
+
+            this.Macros["MajorVersion"] = Bam.Core.TokenizedString.CreateVerbatim("4");
+            this.Macros["MinorVersion"] = Bam.Core.TokenizedString.CreateVerbatim("0");
+            this.Macros["PatchVersion"] = Bam.Core.TokenizedString.CreateVerbatim("1");
+
+            this.CreateHeaderContainer("$(packagedir)/*.h");
+            var source = this.CreateCxxSourceContainer("$(packagedir)/tinyxml2.cpp");
+            source.PrivatePatch(settings =>
+                {
+                    var compiler = settings as C.ICommonCompilerSettings;
+                    var cxxCompiler = settings as C.ICxxOnlyCompilerSettings;
+                    cxxCompiler.ExceptionHandler = C.Cxx.EExceptionHandler.Asynchronous;
+                    cxxCompiler.LanguageStandard = C.Cxx.ELanguageStandard.Cxx11;
+
+                    var visualCCompiler = settings as VisualCCommon.ICommonCompilerSettings;
+                    if (null != visualCCompiler)
+                    {
+                        visualCCompiler.WarningLevel = VisualCCommon.EWarningLevel.Level4;
+                        compiler.PreprocessorDefines.Add("TINYXML2_EXPORT");
+                    }
+
+                    var mingwCompiler = settings as MingwCommon.ICommonCompilerSettings;
+                    if (null != mingwCompiler)
+                    {
+                        mingwCompiler.AllWarnings = true;
+                        mingwCompiler.ExtraWarnings = true;
+                        mingwCompiler.Pedantic = true;
+                        compiler.PreprocessorDefines.Add("TINYXML2_EXPORT");
+                    }
+
+                    var gccCompiler = settings as GccCommon.ICommonCompilerSettings;
+                    if (null != gccCompiler)
+                    {
+                        gccCompiler.AllWarnings = true;
+                        gccCompiler.ExtraWarnings = true;
+                        gccCompiler.Pedantic = true;
+                        // brute force visibility
+                        gccCompiler.Visibility = GccCommon.EVisibility.Default;
+                    }
+
+                    var clangCompiler = settings as ClangCommon.ICommonCompilerSettings;
+                    if (null != clangCompiler)
+                    {
+                        clangCompiler.AllWarnings = true;
+                        clangCompiler.ExtraWarnings = true;
+                        clangCompiler.Pedantic = true;
+                        // brute force visibility
+                        clangCompiler.Visibility = ClangCommon.EVisibility.Default;
+                    }
+                });
+
+            this.PublicPatch((settings, appliedTo) =>
+                {
+                    var compiler = settings as C.ICommonCompilerSettings;
+                    if (null != compiler)
+                    {
+                        compiler.IncludePaths.AddUnique(this.CreateTokenizedString("$(packagedir)"));
+                        if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.Windows) && appliedTo != this)
+                        {
+                            // applied everywhere it's not built
+                            compiler.PreprocessorDefines.Add("TINYXML2_IMPORT");
+                        }
+                    }
+                });
+
+            if (this.Linker is VisualCCommon.LinkerBase)
+            {
+                this.LinkAgainst<WindowsSDK.WindowsSDK>();
+            }
+        }
+    }
+
+    namespace tests
+    {
+        [Bam.Core.ModuleGroup("Thirdparty/TinyXML2/tests")]
+        sealed class XMLTest :
+            C.Cxx.ConsoleApplication
+        {
+            protected override void
+            Init(
+                Bam.Core.Module parent)
+            {
+                base.Init(parent);
+
+                var source = this.CreateCxxSourceContainer("$(packagedir)/xmltest.cpp");
+                source.PrivatePatch(settings =>
+                    {
+                        var cxxCompiler = settings as C.ICxxOnlyCompilerSettings;
+                        cxxCompiler.ExceptionHandler = C.Cxx.EExceptionHandler.Asynchronous;
+                        cxxCompiler.LanguageStandard = C.Cxx.ELanguageStandard.Cxx11;
+
+                        var visualCCompiler = settings as VisualCCommon.ICommonCompilerSettings;
+                        if (null != visualCCompiler)
+                        {
+                            visualCCompiler.WarningLevel = VisualCCommon.EWarningLevel.Level4;
+                        }
+
+                        var mingwCompiler = settings as MingwCommon.ICommonCompilerSettings;
+                        if (null != mingwCompiler)
+                        {
+                            mingwCompiler.AllWarnings = true;
+                            mingwCompiler.ExtraWarnings = true;
+                            mingwCompiler.Pedantic = true;
+                        }
+
+                        var gccCompiler = settings as GccCommon.ICommonCompilerSettings;
+                        if (null != gccCompiler)
+                        {
+                            gccCompiler.AllWarnings = true;
+                            gccCompiler.ExtraWarnings = true;
+                            gccCompiler.Pedantic = true;
+                        }
+
+                        var clangCompiler = settings as ClangCommon.ICommonCompilerSettings;
+                        if (null != clangCompiler)
+                        {
+                            clangCompiler.AllWarnings = true;
+                            clangCompiler.ExtraWarnings = true;
+                            clangCompiler.Pedantic = true;
+                        }
+                    });
+
+#if false
+                this.CompileAndLinkAgainst<TinyXML2Static>(source);
+#else
+                this.CompileAndLinkAgainst<TinyXML2Dynamic>(source);
+
+                this.PrivatePatch(settings =>
+                    {
+                        var gccLinker = settings as GccCommon.ICommonLinkerSettings;
+                        if (null != gccLinker)
+                        {
+                            gccLinker.CanUseOrigin = true;
+                            gccLinker.RPath.AddUnique("$ORIGIN");
+                        }
+                    });
+#endif
+            }
+        }
+
+        sealed class TestRuntime :
+            Publisher.Collation
+        {
+            protected override void
+            Init(
+                Bam.Core.Module parent)
+            {
+                base.Init(parent);
+
+                var app = this.Include<XMLTest>(C.Cxx.ConsoleApplication.Key, EPublishingType.ConsoleApplication);
+                this.Include<TinyXML2Dynamic>(C.Cxx.ConsoleApplication.Key, ".", app);
+            }
+        }
+    }
+}
