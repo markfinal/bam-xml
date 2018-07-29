2319b31d1e2f3d104f43081b5d4f741222ffa3c2
Mark Final <markfinal@hotmail.com>
2018-07-29 09:36:20 +0100

    Updated for bam v2

--- packages/tinyxml2-3.0.0/bam/Scripts/Tests.cs
+++ packages/tinyxml2-3.0.0/bam/Scripts/Tests.cs
@@ -119,7 +119,7 @@ namespace TinyXML2
                 base.Init(parent);
 
                 this.SetDefaultMacrosAndMappings(EPublishingType.ConsoleApplication);
-                this.IncludeAllModulesInNamespace("TinyXML2.tests", C.Cxx.ConsoleApplication.Key);
+                this.IncludeAllModulesInNamespace("TinyXML2.tests", C.Cxx.ConsoleApplication.ExecutableKey);
             }
         }
     }
